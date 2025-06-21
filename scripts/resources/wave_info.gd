6ad6021a296f4161432aeba503c15c996659f23f
Arox315 <127292374+Arox315@users.noreply.github.com>
2025-06-21 17:31:39 +0200

    feat: Add WaveInfo resource for enemy waves configuration

---
+++ scripts/resources/wave_info.gd
@@ -0,0 +1,20 @@
+class_name WaveInfo
+extends Resource
+
+const HUNGRY_HIPPO = preload("res://prefabs/enemies/path_follow.tscn")
+
+@export var enemies := {
+	"H":HUNGRY_HIPPO
+}
+
+@export var waves := [
+	{
+		"wave_number":1,
+		"enemies_pattern":"3H_8H",
+		"enemy_spawn_delay":0.5,
+		"enemy_pattern_spawn_delay":5.0
+	}
+]
+
+@export var current_wave := 1
+@export var max_waves := 10
