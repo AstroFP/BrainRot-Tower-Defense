2baa68fb75b6153cbebc178b4b5a8633f16430b1
Arox315 <127292374+Arox315@users.noreply.github.com>
2025-06-23 21:09:06 +0200

    feat: Add tower price display on UI and refactor Buy Menu structure

--- scripts/buy_menu_manager.gd
+++ scripts/buy_menu_manager.gd
@@ -2,23 +2,31 @@ class_name BuyMenuManager
 extends Control
 
 # towers
-var test_tower_1 = preload("res://resources/test_tower.tres")
-var test_tower_2 = preload("res://resources/test_tower2.tres")
+var towers:= [
+	preload("res://resources/test_tower.tres"),
+	preload("res://resources/test_tower2.tres")
+]
 
 signal selected_tower(tower: TowerStats)
 
 # tower buy buttons
-@onready var buy_tower_btn_1 = $TowerBuyMenu/TowerBuyMenuItems/BuyMenuTowerList/BuyMenuTowerListRow1/BuyTowerBtn1
-@onready var buy_tower_btn_2 = $TowerBuyMenu/TowerBuyMenuItems/BuyMenuTowerList/BuyMenuTowerListRow1/BuyTowerBtn2
+@onready var buy_tower_btn_1 = $TowerBuyMenu/TowerBuyMenyItemsWrapper/TowerBuyMenuItems/BuyMenuTowerList/BuyMenuTowerListRow1/TowerBuyBanner/BuyTowerBtn1
+@onready var buy_tower_btn_2 = $TowerBuyMenu/TowerBuyMenyItemsWrapper/TowerBuyMenuItems/BuyMenuTowerList/BuyMenuTowerListRow1/TowerBuyBanner2/BuyTowerBtn2
+
 
 # menu toggle button
 @onready var toggle_tower_buy_menu_btn = $ToggleTowerBuyMenuBtn
 
 @onready var tower_buy_menu = $TowerBuyMenu
 @onready var playable_area = $"../PlayableArea"
+@onready var tower_name_label = $TowerBuyMenu/TowerBuyMenyItemsWrapper/TowerBuyMenuItems/TowerNameLabel
+@onready var buy_menu_tower_list = $TowerBuyMenu/TowerBuyMenyItemsWrapper/TowerBuyMenuItems/BuyMenuTowerList
+
 
 func _ready():
 	tower_buy_menu.visible = false
+	set_tower_prices()
+
 
 func _process(_delta):
 	pass
@@ -32,19 +40,23 @@ func _input(event):
 
 
 func _on_buy_tower_btn_1_button_down():
-	emit_signal("selected_tower",test_tower_1)
+	emit_signal("selected_tower",towers[0])
+	update_tower_name_label_text(towers[0])
 
 
 func _on_buy_tower_btn_1_pressed():
-	emit_signal("selected_tower",test_tower_1)
+	emit_signal("selected_tower",towers[0])
+	update_tower_name_label_text(towers[0])
 
 
 func _on_buy_tower_btn_2_button_down():
-	emit_signal("selected_tower",test_tower_2)
+	emit_signal("selected_tower",towers[1])
+	update_tower_name_label_text(towers[1])
 
 
 func _on_buy_tower_btn_2_pressed():
-	emit_signal("selected_tower",test_tower_2)
+	emit_signal("selected_tower",towers[1])
+	update_tower_name_label_text(towers[1])
 
 
 func _on_toggle_tower_buy_menu_btn_pressed():
@@ -52,4 +64,13 @@ func _on_toggle_tower_buy_menu_btn_pressed():
 		tower_buy_menu.visible = false
 	else:
 		tower_buy_menu.visible= true
-	
+
+
+func update_tower_name_label_text(tower_stats:TowerStats):
+	tower_name_label.text = tower_stats.name
+
+
+func set_tower_prices():
+	var price_labels = buy_menu_tower_list.find_children("TowerPriceLabel", "Label")
+	for i in range(len(price_labels)):
+		price_labels[i].text = str(towers[i].price)
