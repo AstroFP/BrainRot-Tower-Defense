a2fe739b4ef8fe3310ac7df5fcf594d66899f20d
Arox315 <127292374+Arox315@users.noreply.github.com>
2025-06-25 21:10:09 +0200

    fix: Adjust Resources panel icons textures resolution

--- scenes/ui/resources_panel.gd
+++ scenes/ui/resources_panel.gd
@@ -11,7 +11,7 @@ signal available_money_changed(available_money: int)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
